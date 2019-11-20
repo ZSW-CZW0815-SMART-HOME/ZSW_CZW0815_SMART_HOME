1b26c7de4376989846b4b98c58dd6f8ef3f280b8
domi-exe <dominikadolik6@gmail.com>
2019-11-20 17:03:01 +0100

    used as thread

--- mail/send_message.py
+++ mail/send_message.py
@@ -1,15 +1,19 @@
 import sys
 import runpy
 import os
+import threading
 
 # demo input
 # if correct - everything is ok
 # if incorrect - send warning email
-correct_password = 'test'
-input = sys.argv[1]
+def init(input):
+    correct_password = 'test'
+    if input == correct_password:
+        print('OK')
+    else:
+        os.system('python demo.py')
+        print('Mail sent')
 
-if input == correct_password:
-    print('OK')
-else:
-    os.system('python demo.py')
-    print('Mail sent')
+input = sys.argv[1]
+mail = threading.Thread(target=init, args=(input,))
+mail.start()
