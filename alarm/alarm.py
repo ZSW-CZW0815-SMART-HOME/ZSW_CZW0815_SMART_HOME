77058a3fd4f6da2a92c6cfebab0f8ade06e6991a
mk5135795 <mk5135795@gmail.com>
2019-11-28 09:38:02 +0100

    alarm lcd open

--- alarm/alarm.py
+++ alarm/alarm.py
@@ -1,17 +1,21 @@
 import RPi.GPIO as GPIO 
 from time import sleep 
 
+PIN_LED_ALARM = 1#temp
+PIN_BUZZER = 5
+
 def alarm():
     GPIO.setmode(GPIO.BCM) 
-    GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW) # LED
-    GPIO.setup( 5, GPIO.OUT, initial=GPIO.LOW) # buzzer
+    GPIO.setup(PIN_LED_ALARM, GPIO.OUT, initial=GPIO.LOW) # LED
+    GPIO.setup(PIN_BUZZER,    GPIO.OUT, initial=GPIO.LOW) # buzzer
     
     for i in range(3):
-        GPIO.output(12, GPIO.HIGH) # Turn on LED 
-        GPIO.output( 5, GPIO.HIGH) # Turn on buzzer
+        GPIO.output(PIN_LED_ALARM, GPIO.HIGH) # Turn on LED 
+        GPIO.output(PIN_BUZZER,    GPIO.HIGH) # Turn on buzzer
         sleep(0.5) 
-        GPIO.output( 5, GPIO.LOW) # Turn off buzzer
+        GPIO.output(PIN_BUZZER, GPIO.LOW) # Turn off buzzer
         sleep(0.5) 
+        GPIO.output(PIN_LED_ALARM, GPIO.LOW) # Turn off LED
     GPIO.cleanup()
 
 if __name__ == "__main__":
