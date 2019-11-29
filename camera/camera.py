24a436378724fbfbd09a47dd9c3e0e442a10bb70
234727 <37062075+234727@users.noreply.github.com>
2019-11-29 09:40:56 +0100

    change to function

--- camera/camera.py
+++ camera/camera.py
@@ -1,22 +1,18 @@
 import cv2
-
-# przykładowe dane kamery
-host = '192.168.0.20'
-stream = '1'
-username = 'admin'
-password = 'admin1'
-
-camera = cv2.VideoCapture('rtsp://'+username+':'+password+'@'+host+':80/')
-while True:
-    return_value,image = camera.read()
-    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    cv2.imshow('image',gray)
-    if cv2.waitKey(1)& 0xFF == ord('s'):
-        cv2.imwrite('photo.jpg',image)
-        break
-camera.release()
-cv2.destroyAllWindows()
-
-# https://www.epiphan.com/userguides/pearl/Content/UserGuides/Streaming/capture/sourceIPCameras.htm
-
-#https://stackoverflow.com/questions/49978705/access-ip-camera-in-python-opencv
+def takePhoto():
+    # przykładowe dane kamery
+    host = '192.168.0.20'
+    port = ':80/'	#zmienić 
+    stream = '1'
+    username = 'admin'
+    password = 'admin1'
+	camera = cv2.VideoCapture('rtsp://'+username+':'+password+'@'+host+port)
+	while True:
+		return_value,image = camera.read()
+		gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+		cv2.imshow('image',gray)
+		if cv2.waitKey(1)& 0xFF == ord('s'):
+			cv2.imwrite('photo.jpg',image)
+			break
+	camera.release()
+	cv2.destroyAllWindows()
